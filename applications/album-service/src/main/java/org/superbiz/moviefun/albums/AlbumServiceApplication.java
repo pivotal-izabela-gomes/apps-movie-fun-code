0ac978bbbc3b03501aad651318ee4c91f154bc3b
Dev Floater 110 <pivotal@Dev-Floater-110.local>
2017-08-02 11:29:51 -0400

    Service Discovery lesson

--- applications/album-service/src/main/java/org/superbiz/moviefun/albums/AlbumServiceApplication.java
+++ applications/album-service/src/main/java/org/superbiz/moviefun/albums/AlbumServiceApplication.java
@@ -6,19 +6,24 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.superbiz.moviefun.blobstore.BlobStore;
 import org.superbiz.moviefun.blobstore.S3Store;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class AlbumServiceApplication {
     public static void main(String... args) {
         SpringApplication.run(AlbumServiceApplication.class, args);
     }
 
-    @Value("${s3.accessKey}") String s3AccessKey;
-    @Value("${s3.secretKey}") String s3SecretKey;
-    @Value("${s3.bucketName}") String s3BucketName;
+    @Value("${s3.accessKey}")
+    String s3AccessKey;
+    @Value("${s3.secretKey}")
+    String s3SecretKey;
+    @Value("${s3.bucketName}")
+    String s3BucketName;
 
     @Bean
     public BlobStore blobStore() {

465e9e7c365e79c340c544869d0ea4441149e83e
Dev Floater 110 <pivotal@Dev-Floater-110.local>
2017-08-08 17:07:48 -0400

    Spring Bootification lesson

---
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -0,0 +1,22 @@
+package org.superbiz.moviefun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Created by pivotal on 8/4/17.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean actionServletRegistration(ActionServlet actionServlet) {
+        return new ServletRegistrationBean(actionServlet, "/moviefun/*");
+    }
+}
